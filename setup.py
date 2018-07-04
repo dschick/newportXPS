b442d80f8a2e2888aa58420932cfc268481d81ba
Daniel Schick <schick.daniel@gmail.com>
2018-07-04 15:51:49 +0200

    add setup.py and driver.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='newportXPS',
+      version='0.1',
+      description='driver, utils for Newport XPS Q8',
+      url='https://github.com/dschick/newport-xps',
+      author='dschick',
+      author_email='schick.daniel@gmail.com',
+      license='MIT',
+      packages=['newportXPS'],
+      zip_safe=False)
